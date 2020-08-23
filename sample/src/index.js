a868e7dedb53da89d030bd829e385ec9dda8e2ad
shibli21 <syedshiblimahmud@gmail.com>
2020-08-23 21:37:42 +0600

    Creating GraphQL API

--- sample/src/index.js
+++ sample/src/index.js
@@ -1,1 +1,27 @@
 import { GraphQLServer } from "graphql-yoga";
+
+//type definitions (schema)
+const typeDefs = `
+    type Query {
+        hello : String!
+    }
+`;
+
+//Resolvers
+
+const resolvers = {
+  Query: {
+    hello() {
+      return "Hello World ! I'm Shibli";
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+});
+
+server.start(() => {
+  console.log("The server is running !");
+});
