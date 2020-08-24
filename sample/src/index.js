38bdd425c9ae598361b085d0ce1524c8a072733e
shibli21 <syedshiblimahmud@gmail.com>
2020-08-24 12:29:45 +0600

    GraphQL Scalar Types

--- sample/src/index.js
+++ sample/src/index.js
@@ -3,7 +3,11 @@ import { GraphQLServer } from "graphql-yoga";
 //type definitions (schema)
 const typeDefs = `
     type Query {
-        hello : String!
+      id: ID!  
+      name : String!
+      age : Int!
+      employed :Boolean!
+      gpa: Float
     }
 `;
 
@@ -11,8 +15,20 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    hello() {
-      return "Hello World ! I'm Shibli";
+    id() {
+      return "1";
+    },
+    name() {
+      return "Shibli";
+    },
+    age() {
+      return 22;
+    },
+    employed() {
+      return true;
+    },
+    gpa() {
+      return 4.83;
     },
   },
 };
