575fe600fde28873e390b1fd9857b9bc9400110c
shibli21 <syedshiblimahmud@gmail.com>
2020-08-24 22:49:11 +0600

    added comments and its relations

--- sample/src/index.js
+++ sample/src/index.js
@@ -39,11 +39,39 @@ const posts = [
   },
 ];
 
+const comments = [
+  {
+    id: "101",
+    text: "Good course",
+    author: "1",
+    post: "1",
+  },
+  {
+    id: "102",
+    text: "Glad you enjoyed",
+    author: "2",
+    post: "2",
+  },
+  {
+    id: "103",
+    text: "I got it to work",
+    author: "2",
+    post: "1",
+  },
+  {
+    id: "104",
+    text: "That's good",
+    author: "1",
+    post: "2",
+  },
+];
+
 //type definitions (schema)
 const typeDefs = `
     type Query {
       users(query : String) :[User!]!
       posts(query : String) : [Post!]!
+      comments : [Comment!]!
       me : User!
       post :Post!
 
@@ -54,6 +82,7 @@ const typeDefs = `
       email: String!
       age: Int
       posts : [Post!]!
+      comments : [Comment!]!
     }    
     type Post {
       id: ID!
@@ -61,19 +90,29 @@ const typeDefs = `
       body: String!
       published: Boolean!
       author : User!
+      comments :[Comment!]!
     }    
+    type Comment {
+      id: ID!
+      text: String!
+      author : User!
+      post : Post!
+    }
 `;
 
 //Resolvers
 
 const resolvers = {
   Query: {
+    comments(parent, args, ctx, info) {
+      return comments;
+    },
     me() {
       return {
         id: "1213",
         name: "Shibli",
         email: "shibli@gmail.com",
-        // age: 22,
+        age: 22,
       };
     },
     post() {
@@ -113,11 +152,33 @@ const resolvers = {
         return user.id === parent.author;
       });
     },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.post === parent.id;
+      });
+    },
   },
   User: {
     posts(parent, args, ctx, info) {
       return posts.filter((post) => {
         return post.author === parent.id;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.author === parent.id;
+      });
+    },
+  },
+  Comment: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author;
+      });
+    },
+    post(parent, args, ctx, info) {
+      return posts.find((post) => {
+        return post.id === parent.post;
       });
     },
   },
