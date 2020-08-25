784689c3a6366753c8557be3eb9ebf4fd8fe3834
shibli21 <syedshiblimahmud@gmail.com>
2020-08-25 20:01:50 +0600

    Creating Data with Mutations for posts and comments

--- sample/src/index.js
+++ sample/src/index.js
@@ -79,6 +79,8 @@ const typeDefs = `
     }
     type Mutation{
       createUser(name : String! , email: String! , age: Int ) :  User!
+      createPost(title: String! , body: String! ,published: Boolean! , author : ID! ) :Post!
+      createComment(text :String! author: ID! , post:ID!):Comment!
     }
 
     type User {
@@ -168,6 +170,45 @@ const resolvers = {
       };
       users.push(user);
       return user;
+    },
+    createPost(parent, args, ctx, info) {
+      const userExists = users.some((user) => {
+        return user.id === args.author;
+      });
+      if (!userExists) {
+        throw new Error("User Not Found!!");
+      }
+      const post = {
+        id: v4(),
+        title: args.title,
+        body: args.body,
+        published: args.published,
+        author: args.author,
+      };
+      posts.push(post);
+      return post;
+    },
+    createComment(parent, args, ctx, info) {
+      const userExists = users.some((user) => {
+        return user.id === args.author;
+      });
+      const postExists = posts.some(
+        (post) => post.id === args.post && post.published
+      );
+      if (!userExists && !postExists) {
+        throw new Error("Unable to find user and post!!");
+      }
+
+      const comment = {
+        id: v4(),
+        text: args.text,
+        author: args.author,
+        post: args.post,
+      };
+
+      comments.push(comment);
+
+      return comment;
     },
   },
   Post: {
